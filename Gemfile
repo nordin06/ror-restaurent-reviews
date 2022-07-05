4a4f3be84d2d8d59618259893ac42ac2783551c6
nordin06 <nordin.faraoun@gmail.com>
2022-07-05 14:43:39 +0200

    test repo fin

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'simple_form', github: 'heartcombo/simple_form'
+
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -54,5 +56,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-gem 'simple_form', github: 'heartcombo/simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+puts "cleaning database..."
+Restaurant.destroy_all
+
+puts "creating restaurants..."
+dishoom {name: "Dishoom", adress: "7 rue tonton Nice", stars: 5}
+pizza_east { name: "Pizza East", adress: "15 Rue de l'ariane, Nice", stars: 3}
+[ dishoom, pizza_east].each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+end
+puts "finished"
+
+restaurant = Restaurant.new(name: "mcDo")
+restaurant.save
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
